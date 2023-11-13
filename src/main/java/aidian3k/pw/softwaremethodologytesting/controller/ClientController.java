bfb502a323c043ba8b76eb58244766b5b75301b9
aidian3k <adrian.nowosielski1@gmail.com>
2023-11-13 22:17:39 +0100

    added health-check

--- src/main/java/aidian3k/pw/softwaremethodologytesting/controller/ClientController.java
+++ src/main/java/aidian3k/pw/softwaremethodologytesting/controller/ClientController.java
@@ -1,12 +1,20 @@
 package aidian3k.pw.softwaremethodologytesting.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/client")
+@RequestMapping("/api")
 @Validated
-class ClientController {}
+class ClientController {
+    @GetMapping("/health-check")
+    public ResponseEntity<String> handleHealthCheckCall() {
+        return new ResponseEntity<>("Health-Check", HttpStatus.OK);
+    }
+}
